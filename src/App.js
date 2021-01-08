184d981b7230bbef183d6c57b83b0a5ac945e876
Ryan-Larke <ryan.larke@iress.com>
2021-01-09 00:55:40 +1100

    add portfolio values and securities

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
 import './App.css';
-import { Button } from '@material-ui/core';
+import { Button, TextField } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
 import React, { useState } from 'react';
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+      width: '25ch',
+    },
+  },
+}));
+
 function App() {
+
+  const classes = useStyles();
 
   const [count, setCount] = useState([]);
 
   return (
     <div className="App" style={{ margin: "50px" }}>
       <div>
-        {count.map(() => (
-          <><input />
-            <br /></>
-        ))}
+        <form className={classes.root} noValidate autoComplete="off">
+          <div>
+            <TextField id="standard-basic" label="Current portfolio value" />
+            <TextField id="standard-basic" label="Amount to invest" />
+          </div>
+          {count.map(() => (
+            <>
+              <TextField id="standard-basic" label="Security" />
+              <TextField id="standard-basic" label="Target allocation %" />
+
+              <br /></>
+          ))}
+        </form>
       </div>
 
       <Button variant="contained" color="primary" onClick={() => { setCount([...count, {}]) }}>
         Add security
       </Button>
+
+
 
     </div>
   );
